707861b988bf8196314e75a3459da9a520e0c74f
rijanbasnet14 <rijandon14@gmail.com>
2026-04-30 14:55:50 -0400

    adding list item

--- src/App.js
+++ src/App.js
@@ -2,16 +2,37 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-const welcome = {
-   greeting: 'Hey' ,
-   title: 'React',
-};
+  const list = [
+    {
+   title:'React',
+   url: 'https://react.js.org/',
+   author: 'Jordan Walke',
+   num_comments: 3,
+   points: 4,
+   objectID:0,
+    },
+     {
+   title:'Redux',
+   url: 'https://react.js.org/',
+   author: 'Dan Abramov, Andrew Clark',
+   num_comments: 2,
+   points: 5,
+   objectID:1,
+    },
+  ]
   return (
     <div>
-     <header>{welcome.greeting} {welcome.title}</header>
+     <header>My Hacker Stories</header>
 
      <label htmlFor='search'>Search: </label>
      <input id="search" type="text" />
+
+      <hr/>
+      <ul>
+        {list.map(function(item) {
+          return <li>{item.title}</li>
+        })}
+      </ul>
     </div>
   );
 }
