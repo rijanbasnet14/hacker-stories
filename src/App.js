ddc28437306b29974eb0c2de5dc70812589d7ef4
rijanbasnet14 <rijandon14@gmail.com>
2026-04-30 14:24:51 -0400

    adding title

--- src/App.js
+++ src/App.js
@@ -2,22 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+const welcome = {
+   greeting: 'Hey' ,
+   title: 'React',
+};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <header>{welcome.greeting} {welcome.title}</header>
+
+     <label htmlFor='search'>Search: </label>
+     <input id="search" type="text" />
     </div>
   );
 }
